8e84896b71ae136ebaedb3d8a47fb2227815bc62
Алексей <@DESKTOP-APOFHBD>
2024-04-21 12:24:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using GreeterClientApp;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7141");
+var client=new Greeter.GreeterClient(channel);
+Console.WriteLine("Введите имя: ");
+var name=Console.ReadLine();
+var reply=await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Ответ сервера: {reply.Message}");
+Console.ReadKey();
